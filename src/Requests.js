31306cd6015c6a2ff99cf90dad6d1a66ce95d94d
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-07 23:40:49 +0300

    Modified src/Pages/Home.jsx src/components/Main.jsx src/components/Navbar.jsx files created src/Requests.js file

---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+const key = "8880d08e97e67ce97006abad3b4cfaa4"
+
+
+const requests = {
+    requestPopular : `https://api.themoviedb.org/3/movie/popular?api_key=${key}&&language=en-US&page=1`,
+    requestTopRated : `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestUpcoming : `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestImages : `https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=${key}&language=en-US`,
+    requestLatest : `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`
+
+
+
+}
+
+export default requests;
